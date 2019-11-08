d40bee424a9431c5d75ab406880e4723bce7b9c9
2214537191@qq.com <2214537191@qq.com>
2019-11-08 16:35:33 +0800

    创建项目

---
+++ app/src/main/java/com/huayun/kotlin_android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.huayun.kotlin_android
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : FragmentActivity() {
+    private var data: Int = 0;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        tv_name.text = "哈哈哈"
+    }
+}
